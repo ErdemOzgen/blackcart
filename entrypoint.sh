1dd3363083c6676861913e39e0da7394e1450550
Erdem Ozgen <m.erdemozgen@gmail.com>
2023-08-09 00:30:10 +0300

    fix docker syntax issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 # Start the SSH server if needed
 # /usr/sbin/sshd -D &
-export PATH="/root/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/opt/bin:/usr/bin/core_perl:/usr/games/bin:/root/go/bin:/opt/anaconda3/bin:$PATH"
 
 # If arguments were passed to the container, execute them
 if [ $# -gt 0 ]; then
