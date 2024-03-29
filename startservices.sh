ae7da6a1b790c7cb95dd52e7b4df61d02a57a112
ErdemOzgen <m.erdemozgen@gmail.com>
2024-03-29 13:36:09 +0300

    Add pull cart operations

--- startservices.sh
+++ startservices.sh
@@ -3,6 +3,7 @@
 # Run your other command here
 export PATH=/root/go/bin:/usr/lib/jvm/java-11-openjdk/bin:/work_dir/blackcartenv/bin:/root/go/bin:/sbin:/usr/bin:/root/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/opt/bin:/usr/bin/core_perl:/root/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/opt/bin:/usr/bin/core_perl:/usr/games/bin:
 blackdagger server &
+blackdagger pull cart
 
 # Run gotty in the foreground
 gotty -p 8090 -w --credential blackcart:blackcart /bin/bash
